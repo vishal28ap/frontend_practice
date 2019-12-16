8b0c2ca6f448d78c2c12aa871b4c360961648704
Vishal Thakur <56930443+vishal28ap@users.noreply.github.com>
2019-12-16 20:16:05 +0530

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,10 @@
+//for naming a variable, first character must be either alphabet,_ or $(not number); must not contain mathematical/logical operators or spaces or hyphens(reserved for subtraction) or any reserved word
+//escape character '\' to convert a special character into string
+//string input number to get solution, use eval()
+//modulus operator(%) for integers as well as floating point numbers
+//equal(==) and strict equal(===), data type too equal; concatenation(+)
+
+/*ternery operator
+vaiable = (condition) ? value1(if True) : value2(if False); 
+*/
+
