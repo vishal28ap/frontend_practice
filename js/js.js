f12b9ead6b8ebcd66b8e5d8344e9c112c0f6c058
Vishal Thakur <56930443+vishal28ap@users.noreply.github.com>
2019-12-19 21:30:24 +0530

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,3 +1,10 @@
+/*A constructor is a special method of a class or structure in object-oriented programming
+ that initializes an object of that type. 
+ A constructor is an instance method that usually has the same name as the class, 
+ and can be used to set the values of the members of an object, 
+ either to default or to user-defined values */
+
+
 //for naming a variable, first character must be either alphabet,_ or $(not number); must not contain mathematical/logical operators or spaces or hyphens(reserved for subtraction) or any reserved word
 //escape character '\' to convert a special character into string
 //string input number to get solution, use eval()
@@ -61,4 +68,82 @@ if (result == true){
 else{
 	alert("Thanks for staying with us");
 }
+
+
+/*object vs variable
+object->many values as property:value
+variable->one value for each variable*/
+var person={
+	name: "John",
+	age: 31;
+	favColor:"green",
+	height:183
+};
+
+//access object properties
+//objectName.propertyName or  objectName['propertyName']
+var x= person.age;
+var y= person['name'];	//calculate length = person.name.length
+/*Object Methods*/
+//objectName.methodName()
+/*create multiple objects of similar type*/
+//use constructors
+function person(name, age, color){
+	this.name = name;	//this keyword refers to current object
+	this.age = age;
+	this.favColor = color;
+}
+//create object
+var p1= new person("John", 42, "green");
+var p2= new person("Amy", 25, "yellow");
+//get output
+document.write(p1.age);	//42
+document.write(p2.name);	//Amy
+
+
+
+/*object initialization*/
+//object literal/initializer syntax
+var John= {name: "John", age:25, color:"green"};
+
+//access it
+document.write(John.age);	//25
+document.write(John['color']);	//green
+
+
+
+/*Methods*/
+//functions stored as object properties i.e. similar to functions
+//methodName= function() {code lines}
+//Access method
+//objectName.methodName()	//can be referenced using this keyword as it refers to current object
+
+function person(name, age){
+	this.name= name;
+	this.age = age;
+	this.changeName= function(name){
+		this.name=name;
+	}
+}
+//create an object
+var p= new person("David", 21);
+//change name
+p.changeName("John);
+//verify changes
+document.write(p.name);	//John
+document.write(p['age']);	//21
+
+
+//define function outside constructor and associate it with object
+function person(name, age){
+	this.name= name;
+	this.age= age;
+	this.yearOfBirth = bornYear;
+}
+function bornYear(){
+	return 2016 - this.age;
+}
+//call
+var p = new person("A", 22);
+document.write(p.yearOfBirth());	//1994
 
