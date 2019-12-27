3026916e55ee23018e526bc88cb3b005d456a93b
Vishal Thakur <56930443+vishal28ap@users.noreply.github.com>
2019-12-27 19:17:33 +0530

    Update ECMAScript6.js

--- js/ECMAScript6.js
+++ js/ECMAScript6.js
@@ -249,3 +249,59 @@ console.log(newPerson.name);		//Bob
 
 
 
+/*Destructuring*/
+//unpack values from arrays or properties form objects into distinct variables
+
+//arrays destructuring
+//example1
+let arr=['1','2','3'];
+let [obe, two, three] = arr;
+console.log(one);		//1
+
+//example2
+let a=()=>{
+	return [1, 3, 2];
+};
+let [one, , two]= a();
+console.log(two);		//2
+
+//example3(swap and assignment)
+let a, b, c=4, d=8;
+[a,b =6]=[2];		//a=2, b=6
+[c,d]=[d,c];		//c=8, d=4
+
+
+//object destructuring
+//example1
+let obj={h:100, s:true};
+let {h,s}=obj;
+console.log(h);		//100
+
+//example2(assign without declaration)
+let a,b;
+({a,b}={a:'Hello', b:'Jack'});
+console.log(a+b);		//Hello Jack
+
+//example3
+let {a,b}={a:'Hello', b:'Jack'};
+console.log(a+b);		//Hello Jack
+
+//example4(assign abject to new variable)
+var o={h:42, s:true};
+var{h:foo, s:bar}=o;
+
+console.log(h);		//Error
+console.log(foo);		//42
+
+//example5(assign default values to variables in case value unpacked from object is undefined)
+var obj={id:2,name:'Jack'};
+let{id=10, age=20}=obj;
+console.log(id);		//42
+console.log(age);		//20
+
+
+
+/*Rest & Spread*/
+//rest parameters
+
+//previously
